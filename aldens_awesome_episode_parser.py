3b51af6980c2ee9f3688c69f93f096400e21b08f
Naomi Levine <66423728+nttlevine@users.noreply.github.com>
2020-06-11 23:42:17 -0400

    Updated parser and P5_analysis were uploaded

--- aldens_awesome_episode_parser.py
+++ aldens_awesome_episode_parser.py
@@ -1,5 +1,6 @@
 """Made as a favor for Naomi by Alden Bradford, at 1:00 AM on June 11th 2020."""
 import pandas as pd
+from math import isnan
 
 
 def clean_episode(episode):
@@ -10,13 +11,29 @@ def clean_episode(episode):
     ]
 
 
+def trim_ep_8(episodes):
+    episodes[8] = [line[:41] for line in episodes[8]]
+
+
 def clean_all(episodes):
+    trim_ep_8(episodes)
     return [clean_episode(ep) for ep in episodes]
+
+
+def isnum(string):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
 
 
 def line2scores(line):
     apart = line.split()
-    score = apart[-1]
+    try:
+        score = float(apart[-1])
+    except ValueError:
+        score = float("nan")
     name_parts = [part for part in apart if part[0] not in "0123456789"]
     name = " ".join(name_parts)
     return name, score
@@ -24,6 +41,7 @@ def line2scores(line):
 
 def episode2series(episode):
     pairs = [line2scores(line) for line in episode]
+    pairs = [pair for pair in pairs if not isnan(pair[1])]
     names = [pair[0] for pair in pairs]
     scores = [pair[1] for pair in pairs]
     return pd.Series(index=names, data=scores)
@@ -32,4 +50,4 @@ def episode2series(episode):
 def parse_episodes(episodes):
     episodes = clean_all(episodes)
     data = pd.concat([episode2series(ep) for ep in episodes], axis=1, sort=True)
-    return data
+    return data.astype(float)
